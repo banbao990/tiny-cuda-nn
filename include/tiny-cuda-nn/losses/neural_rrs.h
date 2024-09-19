f16ac03cbf251433338ab1b2c71518f81a7fa320
banbao990 <2369896555@qq.com>
2024-09-19 16:47:52 +0800

    [nrrs] fix offset bugs in training

--- include/tiny-cuda-nn/losses/neural_rrs.h
+++ include/tiny-cuda-nn/losses/neural_rrs.h
@@ -38,6 +38,7 @@
 // #undef BB_TCNN_DEBUG_MODE
 
 namespace tcnn {
+#define NRRS_EPSILON 1e-2f
 
 template <typename T>
 __global__ void
@@ -58,7 +59,8 @@ neural_rrs_loss_L_L2(const uint32_t n_elements, const uint32_t stride, const uin
 					 const float loss_scale, const T *__restrict__ predictions,
 					 const float *__restrict__ targets, float *__restrict__ values,
 					 T *__restrict__ gradients, const float *__restrict__ data_pdf = nullptr,
-					 const bool clampOn = false, const float clampMax = 10.0f
+					 const bool clampOn = false, const float clampMax = 10.0f,
+					 const uint32_t step = 1
 #ifdef BB_TCNN_DEBUG_MODE
 					 ,
 					 const uint32_t showLossIndex = 0
@@ -78,55 +80,98 @@ neural_rrs_loss_rrs(const uint32_t n_elements, const uint32_t stride, const uint
 					const uint32_t showLossIndex = 0
 #endif
 ) {
-	const uint32_t i = threadIdx.x + blockIdx.x * blockDim.x;
-	if (i >= n_elements) return;
 
-	const uint32_t prediction_idx = i * stride;
-	const uint32_t target_idx	  = i * 3; // thp: dim = 3
+	const uint32_t thread_idx = threadIdx.x + blockIdx.x * blockDim.x;
+	if (thread_idx >= n_elements) return;
 
-	float rrs_gt = 1.0f;
+	const uint32_t prediction_idx = thread_idx * stride;
+	const uint32_t target_idx	  = thread_idx * 3; // thp: dim = 3
 
-	if (step == 1) {
-		// set all rrs = 1
-	} else if (step == 2) {
-		// step 2: ADRRS
-		const float r  = (float) predictions[prediction_idx + 0];
-		const float g  = (float) predictions[prediction_idx + 1];
-		const float b  = (float) predictions[prediction_idx + 2];
-		const float rp = (float) thp[target_idx + 0];
-		const float gp = (float) thp[target_idx + 1];
-		const float bp = (float) thp[target_idx + 2];
+	float rrs_gt			   = 1.0f;
+	const float rrs_loss_scale = 1.0f;
 
-		rrs_gt = (r * rp + g * gp + b * bp) / 3.0f;
-		// clamp
-		rrs_gt = fmaxf(0.5f, fminf(20.0f, rrs_gt));
+	if (step == 3) {
+		// now the output is rrs
+		rrs_gt = (float) predictions[prediction_idx + 7];
+		// TODO
+	} else {
+
+		// float luminance = 1.0f;
+		if (step == 1) {
+			// set all rrs = 1
+		} else if (step == 2) {
+			// step 2: ADRRS
+			const float r  = (float) predictions[prediction_idx + 0];
+			const float g  = (float) predictions[prediction_idx + 1];
+			const float b  = (float) predictions[prediction_idx + 2];
+			const float rp = (float) thp[target_idx + 0];
+			const float gp = (float) thp[target_idx + 1];
+			const float bp = (float) thp[target_idx + 2];
+
+			rrs_gt = (r * rp + g * gp + b * bp) / 3.0f;
+			// clamp
+			// rrs_gt = fmaxf(0.5f, fminf(20.0f, rrs_gt));
+			// luminance = (0.299f * r + 0.587f * g + 0.114f * b);
+		}
+
+		const uint32_t prediction_rrs_idx = prediction_idx + 6;
+
+		const float prediction_ori = (float) predictions[prediction_rrs_idx];
+		// const float prediction_ori_exp = expf(prediction_ori);
+		// activation: softplus
+		// const float prediction = log1pf(prediction_ori_exp);
+		const float prediction = prediction_ori;
+		const float difference = rrs_loss_scale * (prediction - rrs_gt);
+		const uint32_t n_total = n_elements;
+		const float pdf		   = data_pdf ? data_pdf[target_idx] : 1;
+
+		// const float prediction_sq_plus_epsilon = luminance * luminance + NRRS_EPSILON;
+		const float prediction_sq_plus_epsilon = prediction * prediction + NRRS_EPSILON;
+
+		float v = difference * difference / prediction_sq_plus_epsilon / pdf / n_total;
+
+		float scale = 1.0f;
+		if (clampOn) {
+			scale = v > clampMax ? clampMax / v : 1.0f;
+		}
+		values[prediction_rrs_idx] = scale * v;
+
+		float gradient = 2 * difference / prediction_sq_plus_epsilon / pdf;
+		// softplus
+		// gradient *= prediction_ori_exp / (1.0f + prediction_ori_exp);
+		gradients[prediction_rrs_idx] = (T) (scale * loss_scale * gradient / n_total);
+
+		// if (step == 2) {
+		// 	// d(output)/d(L)
+		// 	for (int i = 0; i < 3; ++i) {
+		// 		float r_thp		 = (float) thp[target_idx + i];
+		// 		float r_gradiant = -2.0f / 3 * r_thp * difference / prediction_sq_plus_epsilon /
+		// pdf; 		gradients[prediction_idx + i] += (T) (scale * loss_scale * r_gradiant /
+		// n_total);
+		// 	}
+		// }
 	}
-
-	const uint32_t prediction_rrs_idx = prediction_idx + 6;
-
-	const float prediction = (float) predictions[prediction_rrs_idx];
-	const float difference = 5.0f * (prediction - rrs_gt);
-	const uint32_t n_total = n_elements;
-	const float pdf		   = data_pdf ? data_pdf[target_idx] : 1;
-
-	const float prediction_sq_plus_epsilon = prediction * prediction + 0.01f;
-
-	float v = difference * difference / prediction_sq_plus_epsilon / pdf / n_total;
-
-	float scale = 1.0f;
-	if (clampOn) {
-		scale = v > clampMax ? clampMax / v : 1.0f;
-	}
-	values[prediction_rrs_idx] = scale * v;
-
-	float gradient				  = 2 * difference / prediction_sq_plus_epsilon / pdf;
-	gradients[prediction_rrs_idx] = (T) (scale * loss_scale * gradient / n_total);
 
 #ifdef BB_TCNN_DEBUG_MODE
-	if (showLossIndex == 3) {
-		for (int i = 0; i < 7; ++i) {
-			values[prediction_idx + i] = values[prediction_idx + 6] / 7;
+	if (showLossIndex != 0) {
+#define IDX (prediction_idx)
+		float sum = 0;
+		if (showLossIndex == 1) {
+			for (int i = 0; i < 3; ++i) {
+				sum += values[IDX + i];
+			}
+		} else if (showLossIndex == 2) {
+			for (int i = 3; i < 6; ++i) {
+				sum += values[IDX + i];
+			}
+		} else if (showLossIndex == 3) {
+			sum = values[IDX + 6];
 		}
+		sum /= 7;
+		for (int i = 0; i < 7; ++i) {
+			values[IDX + i] = sum;
+		}
+#undef IDX
 	}
 #endif
 }
@@ -144,18 +189,19 @@ public:
 		CHECK_THROW(gradients.m() == stride);
 		CHECK_THROW(!data_pdf || data_pdf->m() == dims);
 
-		// only training L, L^2
 		linear_kernel(neural_rrs_loss_L_L2<T>, 0, stream, prediction.n_elements(), stride, dims,
 					  loss_scale, prediction.data(), target.data(), values.data(), gradients.data(),
-					  data_pdf ? data_pdf->data() : nullptr, mClampOn, mClampMax
+					  data_pdf ? data_pdf->data() : nullptr, mClampOn, mClampMax, mStep
 #ifdef BB_TCNN_DEBUG_MODE
 					  ,
 					  mShowLossIndex
 #endif
 		);
 
+		const float *thpPtr = mThp + mOffset * 3; // sizeof(mThp)/sizeof(float) = 3
+
 		linear_kernel(neural_rrs_loss_rrs<T>, 0, stream, prediction.n_elements() / stride, stride,
-					  dims, loss_scale, mStep, mThp, prediction.data(), target.data(),
+					  dims, loss_scale, mStep, thpPtr, prediction.data(), target.data(),
 					  values.data(), gradients.data(), data_pdf ? data_pdf->data() : nullptr,
 					  mClampOn, mClampMax
 #ifdef BB_TCNN_DEBUG_MODE
@@ -166,12 +212,21 @@ public:
 	}
 
 	void update_hyperparams(const json &params) override {
+		mOffset		   = params.value("offset", mOffset);
 		mClampMax	   = params.value("clamp_max", mClampMax);
 		mClampOn	   = params.value("clamp_on", mClampOn);
 		mStep		   = params.value("step", mStep);
 		mShowLossIndex = params.value("show_loss_index", mShowLossIndex);
-		mThp		   = (float *) params.value("thp", (uint64_t) mThp);
-		printf("[neural_rrs_loss] update hyperparams!\n");
+
+		mThp   = (float *) params.value("thp", (uint64_t) mThp);
+		mPdf   = (float *) params.value("pdf", (uint64_t) mPdf);
+		mError = (float *) params.value("error", (uint64_t) mError);
+		mLossSumErrorGPUPtr =
+			(float *) params.value("error_sum_ptr", (uint64_t) mLossSumErrorGPUPtr);
+
+		if (!(params.size() == 1 && params.contains("offset"))) {
+			printf("[NeuralRRSLoss] update hyperparams: %s\n", params.dump().c_str());
+		}
 	}
 
 	json hyperparams() const override {
@@ -186,7 +241,13 @@ public:
 	float mClampMax{500.0f};
 	uint32_t mStep{1};
 	uint32_t mShowLossIndex{0};
+
+	uint32_t mOffset{0}; // offset for read the following data
 	float *mThp;
+	float *mPdf;
+	float *mError;
+
+	float *mLossSumErrorGPUPtr; // the GPU address of the sum of error
 };
 
 template <typename T>
@@ -195,7 +256,7 @@ __global__ void neural_rrs_loss_L_L2(const uint32_t n_elements, const uint32_t s
 									 const T *__restrict__ predictions,
 									 const float *__restrict__ targets, float *__restrict__ values,
 									 T *__restrict__ gradients, const float *__restrict__ data_pdf,
-									 const bool clampOn, const float clampMax
+									 const bool clampOn, const float clampMax, const uint32_t step
 #ifdef BB_TCNN_DEBUG_MODE
 									 ,
 									 const uint32_t showLossIndex
@@ -241,7 +302,7 @@ __global__ void neural_rrs_loss_L_L2(const uint32_t n_elements, const uint32_t s
 
 		const float luminance = (0.299f * r + 0.587f * g + 0.114f * b);
 
-		const float prediction_sq_plus_epsilon = luminance * luminance + 0.01f;
+		const float prediction_sq_plus_epsilon = luminance * luminance + NRRS_EPSILON;
 
 		const float difference = prediction - targets[target_idx];
 
@@ -259,7 +320,7 @@ __global__ void neural_rrs_loss_L_L2(const uint32_t n_elements, const uint32_t s
 	{ // L^2
 		float scale							   = 0.01f;
 		const float prediction				   = (float) predictions[i + 3];
-		const float prediction_sq_plus_epsilon = prediction * prediction + 0.01f;
+		const float prediction_sq_plus_epsilon = prediction * prediction + NRRS_EPSILON;
 		const float difference				   = prediction - targets[target_idx + 3];
 
 		float v = scale * difference * difference / prediction_sq_plus_epsilon / pdf / n_total;
@@ -272,15 +333,6 @@ __global__ void neural_rrs_loss_L_L2(const uint32_t n_elements, const uint32_t s
 		values[i + 3]	 = v;
 		float gradient	 = 2 * difference / prediction_sq_plus_epsilon / pdf;
 		gradients[i + 3] = (T) (scale * loss_scale * gradient / n_total);
-	}
-	{
-#ifdef BB_TCNN_DEBUG_MODE
-		if (showLossIndex == 1) {
-			values[i + 3] = values[i] = values[i] / 2;
-		} else if (showLossIndex == 2) {
-			values[i] = values[i + 3] = values[i + 3] / 2;
-		}
-#endif
 	}
 }
 
@@ -314,7 +366,7 @@ neural_rrs_loss_L_L2_v2(const uint32_t n_elements, const uint32_t stride, const 
 		float b = (float) predictions[prediction_idx + 2];
 
 		const float luminance				   = (0.299f * r + 0.587f * g + 0.114f * b);
-		const float prediction_sq_plus_epsilon = luminance * luminance + 0.01f;
+		const float prediction_sq_plus_epsilon = luminance * luminance + NRRS_EPSILON;
 
 		for (int j = 0; j < 3; ++j) {
 			const uint32_t p_idx   = prediction_idx + j;
@@ -342,7 +394,7 @@ neural_rrs_loss_L_L2_v2(const uint32_t n_elements, const uint32_t stride, const 
 
 			float scale							   = 0.01f;
 			const float prediction				   = (float) predictions[p_idx];
-			const float prediction_sq_plus_epsilon = prediction * prediction + 0.01f;
+			const float prediction_sq_plus_epsilon = prediction * prediction + NRRS_EPSILON;
 			const float difference				   = prediction - targets[target_idx + j];
 
 			float v = scale * difference * difference / prediction_sq_plus_epsilon / pdf / n_total;
